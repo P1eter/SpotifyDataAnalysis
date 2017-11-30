e161fe301806a17e832d83ed4629b4651eb9b21f
Pieter <p.kronemeijer@xs4all.nl>
2017-11-30 14:08:19 +0100

    updated test to python3 and added client secret and id

--- test.py
+++ test.py
@@ -9,31 +9,44 @@ import sys
 import spotipy
 import spotipy.util as util
 
+
+SPOTIPY_REDIRECT_URI='http://localhost:8888/callback'
+SPOTIPY_CLIENT_SECRET='c02f97ddd119470d8e24b290722c7490'
+SPOTIPY_CLIENT_ID='647ffb8c163b4882be0cafe4bf1352d5'
+USERNAME='lh7smhcywr18vctg4xmnoehzu'
+SCOPE='user-library-read'
+
 def show_tracks(tracks):
     for i, item in enumerate(tracks['items']):
         track = item['track']
-        print "   %d %32.32s %s" % (i, track['artists'][0]['name'],
-            track['name'])
+        print("   %d %32.32s %s" % (i, track['artists'][0]['name'],
+            track['name']))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        username = sys.argv[1]
-    else:
-        print "Whoops, need your username!"
-        print "usage: python user_playlists.py [username]"
-        sys.exit()
+    # if len(sys.argv) > 1:
+    #     username = sys.argv[1]
+    # else:
+    #     print("Whoops, need your username!")
+    #     print("usage: python test.py [username]")
+    #     sys.exit()
+    username = USERNAME
 
-    token = util.prompt_for_user_token(username)
+    # token = util.prompt_for_user_token(username)
+    token = util.prompt_for_user_token(username=USERNAME,
+                               scope=SCOPE,
+                               client_id=SPOTIPY_CLIENT_ID,
+                               client_secret=SPOTIPY_CLIENT_SECRET,
+                               redirect_uri=SPOTIPY_REDIRECT_URI)
 
     if token:
         sp = spotipy.Spotify(auth=token)
         playlists = sp.user_playlists(username)
         for playlist in playlists['items']:
             if playlist['owner']['id'] == username:
-                print
-                print playlist['name']
-                print '  total tracks', playlist['tracks']['total']
+                print()
+                print(playlist['name'])
+                print('  total tracks', playlist['tracks']['total'])
                 results = sp.user_playlist(username, playlist['id'],
                     fields="tracks,next")
                 tracks = results['tracks']
@@ -42,4 +55,4 @@ if __name__ == '__main__':
                     tracks = sp.next(tracks)
                     show_tracks(tracks)
     else:
-        print "Can't get token for", username
+        print("Can't get token for", username)
